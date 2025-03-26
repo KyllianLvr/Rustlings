c42375a5b5a26a5bfef0f4e9f64b9587cc1cbdbd
kyllian <kyllian.larvor@gmail.com>
2025-03-26 16:49:00 +0100

    finish strings

--- exercises/09_strings/strings2.rs
+++ exercises/09_strings/strings2.rs
@@ -1,4 +1,4 @@
-// TODO: Fix the compiler error in the `main` function without changing this function.
+// TODO: Fix the compiler error in the `main` function without cha nging this function.
 fn is_a_color_word(attempt: &str) -> bool {
     attempt == "green" || attempt == "blue" || attempt == "red"
 }
@@ -6,7 +6,7 @@ fn is_a_color_word(attempt: &str) -> bool {
 fn main() {
     let word = String::from("green"); // Don't change this line.
 
-    if is_a_color_word(word) {
+    if is_a_color_word(word.as_str()) {
         println!("That is a color word I know!");
     } else {
         println!("That is not a color word I know.");
