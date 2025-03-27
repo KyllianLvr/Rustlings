3671cbfd4837f9e6e6d2da7ce70da1fbb1068740
kyllian <kyllian.larvor@gmail.com>
2025-03-27 16:15:15 +0100

    finish mods

--- exercises/10_modules/modules1.rs
+++ exercises/10_modules/modules1.rs
@@ -5,10 +5,11 @@ mod sausage_factory {
         String::from("Ginger")
     }
 
-    fn make_sausage() {
+    pub fn make_sausage() {
         get_secret_recipe();
         println!("sausage!");
     }
+
 }
 
 fn main() {
